13b4652492327e379cb45f84daf0b53244e6ff9d
22a <meehanp2@tcd.ie>
2016-02-26 22:53:41 +0000

    cleaner style assignment

--- chrome/content.js
+++ chrome/content.js
@@ -6,23 +6,20 @@ document.addEventListener("mousedown", function(event){
   }
 }, true);
 
-//element.style.webkitTransform = "";
-//element.style.MozTransform = "";
-//element.style.msTransform = "";
-//element.style.OTransform = "";
-//element.style.transform = "";
-
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.method === "rotateClickedEl") {
-    if (elem.style.transform.indexOf("rotate") > -1){
-      var a = elem.style.transform.split('rotate(');
+    var currentTrans = elem.style.transform;
+    var res = '';
+    if (currentTrans.indexOf("rotate") > -1){
+      var a = currentTrans.split('rotate(');
       var b = a[1].split('deg)');
-      b[0] = parseInt(b[0]) + parseInt(request.value);
+      b[0] = (parseInt(b[0]) + parseInt(request.value))%360;
       b = b.join('deg)');
-      elem.style.transform = a[0] + 'rotate(' + b;
+      res = a[0] + 'rotate(' + b;
     }
     else {
-      elem.style.transform = ("rotate(" + request.value + "deg)");
+      res = (currentTrans!==''?currentTrans+' ':'') + " rotate(" + request.value + "deg)";
     }
+    elem.style.transform = (res);
   }
 });
