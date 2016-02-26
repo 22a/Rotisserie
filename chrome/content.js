e51b54eca3f2e25ddcd4881cd101172393d45d59
22a <meehanp2@tcd.ie>
2016-02-26 22:17:05 +0000

    added incremental rotation

--- chrome/content.js
+++ chrome/content.js
@@ -6,8 +6,23 @@ document.addEventListener("mousedown", function(event){
   }
 }, true);
 
+//element.style.webkitTransform = "";
+//element.style.MozTransform = "";
+//element.style.msTransform = "";
+//element.style.OTransform = "";
+//element.style.transform = "";
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.method === "rotateClickedEl") {
-    elem.style.transform = ("rotate(" + request.value + "deg)");
+    if (elem.style.transform.indexOf("rotate") > -1){
+      var a = elem.style.transform.split('rotate(');
+      var b = a[1].split('deg)');
+      b[0] = parseInt(b[0]) + parseInt(request.value);
+      b = b.join('deg)');
+      elem.style.transform = a[0] + 'rotate(' + b;
+    }
+    else {
+      elem.style.transform = ("rotate(" + request.value + "deg)");
+    }
   }
 });
