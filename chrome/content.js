5499ae149bab6a2dd9421bea82947a2998d72892
22a <meehanp2@tcd.ie>
2016-02-27 17:49:29 +0000

    fixed images falling off the screen

--- chrome/content.js
+++ chrome/content.js
@@ -6,20 +6,52 @@ document.addEventListener("mousedown", function(event){
   }
 }, true);
 
+function updateRotateProperty(value, callback){
+  var currentTrans = elem.style.transform;
+  var str = '';
+  var newValue = ''
+  if (currentTrans.indexOf("rotate") > -1){
+    var a = currentTrans.split('rotate(');
+    var b = a[1].split('deg)');
+    b[0] = (parseInt(b[0]) + parseInt(value))%360;
+    newValue = b[0];
+    b = b.join('deg)');
+    str = a[0] + 'rotate(' + b;
+  }
+  else {
+    str = (currentTrans!==''?currentTrans+' ':'') + " rotate(" + value + "deg)";
+    newValue = parseInt(value);
+  }
+  callback(str,newValue);
+};
+
+function updateTranslateProperty(value, callback){
+  var currentTrans = elem.style.transform;
+  var str = '';
+  var x = ([180,-180,270,-90].indexOf(value)>-1?-100:0);
+  var y = ([180,-180,-270,90].indexOf(value)>-1?-100:0);
+  if (currentTrans.indexOf("translate") > -1){
+    var a = currentTrans.split('translate(');
+    var b = a[1].split(')');
+    b[0] = x + "%," + y + "%"
+    b = b.join(')');
+    str = a[0] + 'translate(' + b;
+  }
+  else {
+    str = (currentTrans!==''?currentTrans+' ':'') + " translate(" + x + "%," + y + "%)";
+    console.log(str);
+  }
+  callback(str);
+};
+
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.method === "rotateClickedEl") {
-    var currentTrans = elem.style.transform;
-    var res = '';
-    if (currentTrans.indexOf("rotate") > -1){
-      var a = currentTrans.split('rotate(');
-      var b = a[1].split('deg)');
-      b[0] = (parseInt(b[0]) + parseInt(request.value))%360;
-      b = b.join('deg)');
-      res = a[0] + 'rotate(' + b;
-    }
-    else {
-      res = (currentTrans!==''?currentTrans+' ':'') + " rotate(" + request.value + "deg)";
-    }
-    elem.style.transform = (res);
+    elem.style.transformOrigin = "top left";
+    updateRotateProperty(request.value, function(rotate,newValue){
+      elem.style.transform = rotate;
+      updateTranslateProperty(newValue, function(translate) {
+        elem.style.transform = translate;
+      });
+    });
   }
 });
